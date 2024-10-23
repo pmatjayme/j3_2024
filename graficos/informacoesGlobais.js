5cea02c92bd2fb11e398d74eb06add2f4f70ee8f
Tiago Martins Reis <127058546+pmatjayme@users.noreply.github.com>
2024-10-23 19:37:22 -0300

    23.10

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,6 +4,13 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_empregadas} estão
+    empregadas, sendo a taxa de desemprego de ${dados.taxa_de_desemprego}. Por fim, por semana trabalham em média ${dados.tempo_medio_trabalho_por_semana}horas`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo);
 }
 
 vizualizarInformacoesGlobais()
