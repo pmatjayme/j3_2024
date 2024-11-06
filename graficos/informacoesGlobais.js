7505d6db7571c98ece682890d146124d08f24d95
Tiago Martins Reis <127058546+pmatjayme@users.noreply.github.com>
2024-11-06 19:18:38 -0300

    06.11 (reuniao3)

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,15 +4,16 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
 
-    const pessoasMundo = (dados.total_pessoas_mundo / 1e9).toFixed(1)
+    const pessoasMundo = (dados.total_pessoas_mundo / 1e9).toFixed(1);
     const pessoasEmpregadas = (dados.total_pessoas_empregadas / 1e9)
     const taxaDesemprego = (dados.taxa_de_desemprego * 100)
-    const tempoMedio = Math.round(dados.tempo_medio_trabalho_por_semana)
+    const horas = parseInt(dados.tempo_medio_trabalho_por_semana)
+    const minutos = Math.round((dados.tempo_medio_trabalho_por_semana - horas) * 60)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasMundo}</span> bilhões de pessoas e que aproximadamente <span>${pessoasEmpregadas}</span> bilhões estão
-    empregadas, sendo a taxa de desemprego de <span>${taxaDesemprego}</span>%. Por fim, por semana, trabalham em média <span>${tempoMedio}</span> horas.`
+    empregadas, sendo a taxa de desemprego de <span>${taxaDesemprego}</span>%. Por fim, por semana, trabalham em média <span>${horas}</span> horas e <span>${minutos}</span> minutos.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
