4362da918a0c991e74bc94dd4d80cc475d938e82
Tiago Martins Reis <127058546+pmatjayme@users.noreply.github.com>
2024-11-13 18:59:56 -0300

    13.11 (1)

---
+++ graficos/empregos.js
@@ -0,0 +1,24 @@
+async function empregos() {
+    const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-dados-gerais.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeX = Object.keys (dados)
+    const empregos = Object.values (dados)
+
+    const data = [
+        {
+            x: nomeX,
+            y: empregos,
+            type: 'bar'
+        }
+    ]
+
+        const grafico = document.createElement('div')
+        grafico.className = 'grafico'
+        document.getElementById('graficos-container').appendChild(grafico)
+        Plotly.newPlot(grafico, data)
+
+    console.log(dados)
+}
+
+empregos()
