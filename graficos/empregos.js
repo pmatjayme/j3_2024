be1484ce10587741bf586f78499a764e1c9034d1
Tiago Martins Reis <127058546+pmatjayme@users.noreply.github.com>
2024-11-13 19:35:32 -0300

    13.11 (15)

--- graficos/empregos.js
+++ graficos/empregos.js
@@ -7,8 +7,8 @@ async function empregos() {
 
     const data = [
         {
-            x: [nomeX],
-            y: [empregos],
+            x: nomeX,
+            y: empregos,
             type: 'bar'
         }
     ]
@@ -17,7 +17,7 @@ async function empregos() {
         grafico.className = 'grafico'
         document.getElementById('graficos-container').appendChild(grafico)
 
-        Plotly.newPlot('grafico', data);
+        Plotly.newPlot(grafico, data);
         
     console.log(dados)
 }
